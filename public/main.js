bae0c70493603d0654269595d4ea334e93f032b9
EmmYup <emmanuelyupit08@gmail.com>
2022-08-15 19:06:34 -0500

    using electron 11, no bugfix found for v20

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 
 const path = require('path');
+const isDev = require('electron-is-dev');
 
 require('@electron/remote/main').initialize();
 
@@ -15,7 +16,11 @@ function createWindow() {
     },
   });
 
-  win.loadURL('http://localhost:3000');
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
 }
 
 app.on('ready', createWindow);
